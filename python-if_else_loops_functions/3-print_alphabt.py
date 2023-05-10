8fc19cfedc8c847777826369269fbc4ef86f1257
root <gumquat@gmail.com>
2023-05-10 09:34:35 -0700

    :P

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import string
 for index in range(0, 26):
-if index != 4 and i != 16:
-    print("{}".format(string.ascii_lowercase[index]), end="")
+    if index != 4 and i != 16:
+        print("{}".format(string.ascii_lowercase[index]), end="")
