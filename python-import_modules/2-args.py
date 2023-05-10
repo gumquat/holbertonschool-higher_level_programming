8d49300241ee7d7e33ad03c124878e4bac58eaba
root <gumquat@gmail.com>
2023-05-10 14:05:30 -0700

    :P

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,10 @@ if __name__ == "__main__":
     import sys
     a = ':'
     b = 'arguments'
-    if len (sys.argv) == 1:
+    if len(sys.argv) == 1:
         a = '.'
     if len(sys.argv) == 2:
         b = 'argument'
     print("{:d} {:s}{:s}".format(len(sys.argv) - 1, b, a))
     for index in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(index, sys.argv[index]))
-
