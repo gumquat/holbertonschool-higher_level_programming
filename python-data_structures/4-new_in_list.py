8bccde1511acc6a08c0af2b08a082fb9bf19a896
root <gumquat@gmail.com>
2023-05-12 10:53:11 -0700

    :P

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        buffer = []
+        for num in my_list:
+            buffer.append(num)
+        if not idx < 0 and not idx >= len(my_list)
+            buffer[idx] = element
+            return buffer
