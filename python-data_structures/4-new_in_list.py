6d93b591ba65cd0c5306a9163e6fd7450f61cd05
root <gumquat@gmail.com>
2023-05-12 11:00:35 -0700

    buffer is a bad word apparently

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
-        buffer = []
-        for num in buffer:
-            buffer.append(num)
+        _buffer = []
+        for num in _buffer:
+            _buffer.append(num)
         if not idx < 0 and not idx >= len(my_list):
-            buffer[idx] = element
-            return buffer
+            _buffer[idx] = element
+            return _buffer
