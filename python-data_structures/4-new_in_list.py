5a6f54b5d765bef63066215ec81db87e9c8a512d
root <gumquat@gmail.com>
2023-05-12 11:03:51 -0700

    :P

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 def new_in_list(my_list, idx, element):
     if my_list:
         buffer_list = []
-        for num in buffer_list:
+        for num in my_list:
             buffer_list.append(num)
         if not idx < 0 and not idx >= len(my_list):
             buffer_list[idx] = element
-            return buffer_list
+        return buffer_list
