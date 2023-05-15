db482aa84f03277ba96c71be54ddc1c830df16de
root <gumquat@gmail.com>
2023-05-15 12:52:46 -0700

    :P

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == '':
-        return (None, len(sentence))
+        return (0, None)
     else:
         vagina_ass_of_lucifer = (len(sentence), sentence[0])
         
