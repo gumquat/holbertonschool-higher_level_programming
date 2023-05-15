3dc53ebb23f7b93d80b46742970e651d436b750b
root <gumquat@gmail.com>
2023-05-15 11:21:57 -0700

    :P

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[]):
     for row in matrix:
-        for element in row:
-            print(element, end=' ')
-        print()
+        for index in range(len(row) - 1):
+            print("{:d}".format(row[index]), end=' ')
+        if len(row) > 0:
+            print("{:d}".format(row[len(row) - 1]))
+        else:
+            print()
