34bb886818bee8f82b2e61409a225a98485d8f77
root <gumquat@gmail.com>
2023-05-15 12:19:31 -0700

    :P

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = (69, 420)
+    new_tuple = (0, 0)
     a = 0
     b = 0
 
-    if len(tuple_a) or len(tuple_b) < 1:
-        new_tuple = tuple_a = tuple_b = 0, 0
+    if len(tuple_a) or len(tuple_b) > 0:
+        a += tuple_a[0]
+        a += tuple_b[0]
+        new_tuple = (a, b)
     elif len(tuple_a) or len(tuple_b) > 1:
-        a = tuple_a[0]
-        b = tuple_b[0]
+        b += tuple_a[1]
+        b += tuple_b[1]
         new_tuple = (a, b)
     
     return new_tuple
