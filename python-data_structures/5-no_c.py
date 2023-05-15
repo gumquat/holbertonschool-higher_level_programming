24ca5a9c37a02d75c75b15f69bc892f8d251390e
root <gumquat@gmail.com>
2023-05-15 10:56:13 -0700

    :P

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    buffer_string = " "
+    buffer_string = ""
     if my_string:
         for char in my_string:
-            if char != "c" or char != "C":
+            if char != "c" and char != "C":
                 buffer_string += char
     return buffer_string
