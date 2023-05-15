a189c58cb432ed02ca0088f8438e801f5f5c25e9
root <gumquat@gmail.com>
2023-05-15 13:05:37 -0700

    :P

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,11 +4,8 @@ def max_integer(my_list=[]):
 
     if ((len(my_list)) == 0):
         return (None)
-    else: 
+    else:
         for index in my_list:
             if (biggest_boy < index):
                     biggest_boy = index
             return biggest_boy
-
-
-    
