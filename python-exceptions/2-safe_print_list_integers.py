97a5e46383c34df7eab235a99b4658af671b3af6
root <gumquat@gmail.com>
2023-05-16 15:24:31 -0700

    :P

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             num += 1
         except (IndexError, TypeError, ValueError, NameError):
             continue
-        
+
     print()
     return num
