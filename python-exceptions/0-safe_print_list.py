0f11683e0240d17239cd7a77470c404b45e45c25
root <gumquat@gmail.com>
2023-05-16 15:01:55 -0700

    :P

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     idx = 0
-    for idx in range (0, x):
+    for idx in range(0, x):
         try:
             print(my_list[idx], end='')
             idx += 1
