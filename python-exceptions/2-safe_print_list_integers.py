5b31494b4e0e9666df0116b950f5851a27f233c1
root <gumquat@gmail.com>
2023-05-16 15:23:56 -0700

    :P

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[idx]), end="")
             num += 1
-        except (IndexError, TypeError, ValueError, NameError):           
-   
-            print()
+        except (IndexError, TypeError, ValueError, NameError):
+            continue
+        
+    print()
     return num
