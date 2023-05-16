af5039bdbb0cb094fac05944921079f55fbe0ba6
root <gumquat@gmail.com>
2023-05-16 11:39:23 -0700

    :P

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum = 0
-    for num in set(my_list):
-        sum += num
-    return sum
+    set(my_list)
+    for num in my_list:
+        return sum
