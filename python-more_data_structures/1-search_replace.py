dcb76baeb209c41942eb9cecf458d8e83ca0d6a2
root <gumquat@gmail.com>
2023-05-16 11:28:00 -0700

    :P

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]  #this copies the list to the new list
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
