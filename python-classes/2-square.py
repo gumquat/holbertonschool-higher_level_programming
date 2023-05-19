925a583f70a13a6f847a675794e89922b6398a21
root <gumquat@gmail.com>
2023-05-19 06:55:20 -0700

    :P

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""class for squares (haha thats you):"""
+"""
+square
+"""
 
 
 class Square:
-    """this class defines squares with size"""
+    """this class defines a square"""
     def __init__(self, size=0):
-        self._size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self, size):
+        self.area = size * size
+    pass
