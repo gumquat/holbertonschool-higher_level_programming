d395a6f5c558428ccdb4e4ed0b0e99ae1cb95e0f
root <gumquat@gmail.com>
2023-05-19 07:15:47 -0700

    :P

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,5 +10,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self, size):
-        self.area = size * size
+        self.__area = self.__size ** 2
+        return self.__area
