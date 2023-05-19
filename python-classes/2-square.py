c2c5475174319a2b0a378cabfa57dc0796e03762
root <gumquat@gmail.com>
2023-05-19 06:40:00 -0700

    :P

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""an empty class for squares (haha thats you):"""
+
+
+class Square:
+    """this class defines squares"""
+    def __init__(self, size=0):
+        """this inits squares"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    self._size = size
