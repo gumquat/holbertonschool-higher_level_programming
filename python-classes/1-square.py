9f0d3bc8bebdecaa86dac3c6409184906e9b19a3
root <gumquat@gmail.com>
2023-05-19 06:33:37 -0700

    :P

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,11 +3,7 @@
 
 
 class Square:
-    """this class defines squares"""
+    """this class defines a square"""
     def __init__(self, size=0):
-        """this initializes sqaures"""
-        if type(size) is not int:
-            raise TypeError("size can't be an integer")
-        if size < 0:
-            raise ValueError("size can't be =< 0")
-        self._size = size
+        self.__size = size
+    pass
