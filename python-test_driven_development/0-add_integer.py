fd8ae5e9d878867daab292859059f65089550708
root <gumquat@gmail.com>
2023-05-22 09:50:49 -0700

    :P

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+0-add_integer.py
+prints the sum of two arguments if they are ints/floats
+"""
+
+
+def add_integers(a, b=98):
+    """
+    Return sum of arguments
+
+    check if data passed are both type int
+
+    arguments are typcasted as ints for the return
+
+    Raises: 
+        TypeError: If either argument is not type int/float
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
