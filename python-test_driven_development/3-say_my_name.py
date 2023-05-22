5dfee2a8fc6bbbd1f3d7e3b3ebc915e4dd48d877
root <gumquat@gmail.com>
2023-05-22 11:39:05 -0700

    :P

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 3-say_my_name.py
+"""def for name printing function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints "My name is <first name> <last name>"
+
+    Args:
+        first_name
+        last_name=""
+
+    Raises:
+        TypeError if args are not strings
+
+    Return:
+        None
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
