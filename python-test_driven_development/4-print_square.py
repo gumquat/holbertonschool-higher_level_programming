ea0807b823bc2d22fc38cb28d0a7d9b6988967f9
root <gumquat@gmail.com>
2023-05-22 12:25:57 -0700

    :P

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# 4-print_square.py
+"""square printing function"""
+
+
+def print_square(size):
+    """
+    prints a square of specified size
+
+    Args:
+        size
+
+    Raises:
+        TypeError, ValueError,
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#")
+        for ii in range(size):
+            print("")
