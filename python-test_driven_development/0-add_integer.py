95208cc3482aafc4d61103f350750f68080887f7
root <gumquat@gmail.com>
2023-05-22 09:41:00 -0700

    :P

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,7 +6,8 @@ prints the sum of two arguments if they are ints/floats
 
 
 def add_integers(a, b=98):
-    """ check if data passed are both type int """
+    """
+    check if data passed are both type int """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
