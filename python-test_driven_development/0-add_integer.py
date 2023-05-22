f9993b37b74681aa10d50c7ca9f33ac7ff0d0693
root <gumquat@gmail.com>
2023-05-22 09:33:37 -0700

    :P

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_integers(a, b=98):
     """check if data passed are both type int"""
+
+    
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
