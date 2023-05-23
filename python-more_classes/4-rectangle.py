68eae9bcae1ca44ca3f7873062011c14065cdc65
root <gumquat@gmail.com>
2023-05-23 13:42:47 -0700

    :P

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+"""
+CLASS defining rectangles
+"""
+
+
+class Rectangle:
+    """
+    this defines rectangles
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width > 0 and self.height > 0:
+            return ((self.width * 2) + (self.height * 2))
+        else:
+            return 0
+
+    def __str__(self):
+        if self.width > 0 and self.height > 0:
+            return '\n'.join(['#' * self.width for _ in range(self.height)])
+        else:
+            return ''
+        
+    def __repr__(self):
+        """
+        returns a literal string with quotes for humans
+        """
+        return"Rectangle(" + str(self.__width)\
+        + ", "\
+        + str(self.__height) + ")"
