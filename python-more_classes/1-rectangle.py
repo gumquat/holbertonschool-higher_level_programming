08a5449aa2b2b0f206f8bd235222ed58b5b1e888
root <gumquat@gmail.com>
2023-05-23 12:01:47 -0700

    :P

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,6 +16,10 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
+    
+    @property
+    def height(self):
+        return self.__height
 
     @width.setter
     def width(self, value):
@@ -25,10 +29,6 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
 
     @height.setter
     def height(self, value):
