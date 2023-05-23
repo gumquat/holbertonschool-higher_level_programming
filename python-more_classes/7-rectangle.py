7ee06837be1d52203fd0b5fb957693cc10faf25c
root <gumquat@gmail.com>
2023-05-23 14:34:19 -0700

    :P

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+"""
+CLASS defining rectangles
+"""
+
+
+class Rectangle:
+    """
+    this defines rectangles
+    """
+
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width > 0 and self.height > 0:
+            return ((self.width * 2) + (self.height * 2))
+        else:
+            return 0
+
+    def __str__(self):
+        if self.width > 0 and self.height > 0:
+            return '\n'.join([str(self.print_symbol) * self.__width] * self.__height)
+
+    def __repr__(self):
+        """
+        returns a literal string with quotes for humans
+        """
+        return ("Rectangle ({}, {})".format(self.width, self.height))
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
