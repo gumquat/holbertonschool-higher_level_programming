a315a9fe9459afe8cec8b4042e283dc9257606c8
root <gumquat@gmail.com>
2023-05-29 10:22:56 -0700

    :P

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usrbin/python3
+"""
+func that returns true if is an instance
+or if object is an instace of the class its inherited from
+otherwise false
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns true if obj is instance of class
+    """
+    return isinstance(obj, a_class)
