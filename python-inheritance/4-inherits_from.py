c81a35d7c7c3c1658cfbacaba1f71034997017e3
root <gumquat@gmail.com>
2023-05-29 10:32:15 -0700

    :P

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,11 +4,15 @@ func that returns true is obj is an instance of a class
 that inherited from the specified class
 
 otherwise false
+
+prototype: def inherits_from()
 """
 
 
 def inherits_From(obj, a_class):
     """
     returns true if obj is instance of parent class
+
+    otherwise false
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
