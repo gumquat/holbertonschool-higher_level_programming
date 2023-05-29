3952b779338d6d25953211870b30804164662426
root <gumquat@gmail.com>
2023-05-29 12:00:21 -0700

    :P

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,58 +1,14 @@
 #!/usr/bin/python3
-"""
-base geometry 10 makin squaaares
-instantiate squares with width & height
-width & height : private and positive integers validated
-by integer_validator
-print() prints and str() returns specified rectangle
-AAAAHHH okay here i go
-return uhhhhh
-"""
-
-
-BaseGeometry = __import__('9-base_geometry').BaseGeometry
-# DO NOT update this to 9-base_geometry! itll fuck up the checker
-
-
-class Rectangle(BaseGeometry):
-    """
-    Defines Rectangle Class
-    """
-    def __init__(self, width, height):
-        """
-        init method
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        returns area
-        """
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        """
-        returns visually printed rectangle via string e.g. fraction
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+"""defines squares (subclass of rectangles)"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Defines square class
-    """
+    """defines square, subclass of rectangle"""
+
     def __init__(self, size):
-        """
-        init method
-        """
+        """init method"""
+        # super() creates a temporary instance of the obj
+        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """
-        returns size
-        """
-        return (self.__size)
