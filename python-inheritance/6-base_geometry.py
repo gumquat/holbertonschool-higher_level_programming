ed9f5df54539a36f5cfc69b739037238d0310e8b
root <gumquat@gmail.com>
2023-05-29 10:45:17 -0700

    :P

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" empty class called BasseGeometry"""
+
+
+class BaseGeometry:
+    "class defines BaseGeometry"
+    def area(self):
+        """Public Instance Method: Area"""
+        raise Exception("are() is not implemented")
