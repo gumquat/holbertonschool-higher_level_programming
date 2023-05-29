8c02b9b2178de8387f06cfddd6a52cbad3d5af73
root <gumquat@gmail.com>
2023-05-29 11:31:53 -0700

    :P

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+instantiate with width & height
+wdth & height : private and positive integers validated 
+by integer_validator
+print() prints and str() returns specified rectangle
+AAAAHHH okay here i go
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Defines Rectangle Class
+    """
+    def __init__(self, width, height):
+        """
+        init method
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        returns area
+        """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """
+        returns visually printed rectangle via string e.g. fraction
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
