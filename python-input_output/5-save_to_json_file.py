da2f8bb838006b8c52fda211ef102d643330189b
root <gumquat@gmail.com>
2023-05-30 12:44:04 -0700

    :P

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+func that writes an pbj to a text file using json represenstation
+"""
+
+
+import json
+
+
+def save_to_jason_file(my_obj, filename):
+    """
+    here go dat boi oml he schmovin
+    """
+    with open(filename, mode="w", encoding="json") as booger:
+        json.dump(my_obj, booger)
