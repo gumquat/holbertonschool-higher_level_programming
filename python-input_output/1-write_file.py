764b352bdd9b3b76d13e56f8c9686c7fef727765
root <gumquat@gmail.com>
2023-05-30 11:55:24 -0700

    :P

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def write_file(filename="", text=""):
     """
     write stuff
     """
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, mode="w", encoding="utf-8") as file:
         counter += 1
         # return file.write(text)
         return counter
