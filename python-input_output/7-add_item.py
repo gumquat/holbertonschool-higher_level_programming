843781ca67f214edb1d4551124f36b1cc3fd4515
root <gumquat@gmail.com>
2023-05-30 14:08:14 -0700

    :P

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-7-add_item
+"""7-add_item
 write a script that adds all arguments to a python list, then save to file
 """
 import sys
-import json
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -14,11 +10,10 @@ fname = "add_item.json"
 MyList = []
 
 try:
-        data = load_from_json_file('add_item.json')
+    MyList = load_from_json_file(fname)
 except:
-    data = []
+    pass
 
-new_items = sys.argv[1:]
-updated_data = data + new_items
-
-save_to_json_file(updated_data, 'add_item.json')
+for i in range(1, len(sys.argv)):
+    MyList.append(sys.argv[i])
+save_to_json_file(MyList, fname)
