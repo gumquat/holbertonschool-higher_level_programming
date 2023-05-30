b1a9e2b580068faa82b4b841b1b67a123a318d9b
root <gumquat@gmail.com>
2023-05-30 12:15:35 -0700

    :P

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+func that appends a string at the end of a file
+returns characters added
+"""
+
+def append_write(filename="", text=""):
+    """
+    append file with new line of text
+    """
+    with open(filename, mode="a", encoding='utf-8') as append:
+        counter = 0
+        for element in text:
+                counter += 1
+        append.writelines(text)
+        return counter
