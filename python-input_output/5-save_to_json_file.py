7460948a83ec3f3d043321641ef7a02d46f9750b
root <gumquat@gmail.com>
2023-05-30 13:29:15 -0700

    :P

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,6 +10,6 @@ import json
 
 
 def save_to_jason_file(my_obj, filename):
-    """here go dat boi oml he schmovin"""
+    """Write an object to a text file using JSON"""
     with open(filename, mode="w", encoding="utf-8") as filefunc:
         json.dump(my_obj, filefunc)
