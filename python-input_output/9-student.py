66d1442cee78fe6947ccf2364f285ad017f8f536
root <gumquat@gmail.com>
2023-05-30 14:24:40 -0700

    :P

---
+++ python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+9-student
+make a class that defines student by:
+first name
+last name
+age
+"""
+
+
+class Student:
+    """
+    student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
