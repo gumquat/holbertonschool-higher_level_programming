244d27731a234af4b202bd366b9fcf279800f1c6
root <gumquat@gmail.com>
2023-05-30 13:36:49 -0700

    :P

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""" 5-save_to_json_file Module """
+"""
+5-save_to_json_file
+func that writes an obj to a file using json representation
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Write an obj to xt file using JSON."""
-    with open(filename, mode='w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
+    """
+    write an obj to txt file using jason
+    """
+    with open(filename, mode='w', encoding='utf-8') as filefunc:
+        json.dump(my_obj, filefunc)
