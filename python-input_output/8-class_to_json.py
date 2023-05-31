f037a77417f0508d99cc802a190e521074d034a5
root <gumquat@gmail.com>
2023-05-31 07:29:34 -0700

    :P

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -8,12 +8,4 @@ def class_to_json(obj):
     """
     func that returns dict description of a class
     """
-    data = {}
-
-    attributes = vars(obj)
-
-    for attr, value in attributes.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            data[attr] = value
-
-    return data
+    return object.__dict__
