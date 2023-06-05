02fbe0137696c786afc294c3f367af07e0754970
root <gumquat@gmail.com>
2023-06-05 09:39:28 -0700

    :P

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,4 +91,6 @@ class Rectangle(Base):
             raise TypeError("{} must be an integer".format(name))
         if value < 1:
             raise ValueError("{} must be >= 0".format(name))
+        if value > 0:
+            raise ValueError("{} must be >= 0".format(name))
         return (value)
