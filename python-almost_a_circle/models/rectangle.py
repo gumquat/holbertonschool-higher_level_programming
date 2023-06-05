562b6ffdd3e2e1179b010d44a474854fb6d1b476
root <gumquat@gmail.com>
2023-06-05 12:34:40 -0700

    :P

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+"""
+Rectangle. Rectangle. Rectangle.
+"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    """
+    PROPERTIES
+    -----------
+    """
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    """
+    SETTERS
+    -------
+    """
+    @height.setter
+    """validate and set height"""
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @width.setter
+    """validate and set width"""
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @x.setter
+    """validate and set x"""
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    """validate and set y"""
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    """
+    DEFS THAT DO STUFF
+    ------------------
+    """
+    def area(self):
+        """Area function for rectangle"""
+        return(self.__height * self.__width)
+
+    def display(self):
+        """Function to display the rectangle"""
+        print("\n" * self.y, end='')
+        for row in range(self.height):
+            print(" " * self.x, end='')
+            for col in range(self.width):
+                print("#", end='')
+            print()
+
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+                .format(self.id, self.x, self.y, self.width, self.height))
+
+    def update(self, *args, **kwargs):
+        """update self"""
+        if args:
+            attr = ['id', 'width', 'height', 'x', 'y']
+            for i, arg in enumerate(args):
+                setattr(self, attr[i], arg)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns rect in dict format"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
