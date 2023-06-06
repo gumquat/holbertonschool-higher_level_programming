d8a9756319e6a0c6f0c99c925c2008e8259efd79
root <gumquat@gmail.com>
2023-06-06 11:56:42 -0700

    :P

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python3
-""" Class Test Cases"""
+""" Base Test Cases"""
 import unittest
-
 from models.base import Base
-from models.rectangle import Rectangle as Rectangle
-# from models.square import Square as Square
+from unittest.mock import patch
+from io import StringIO
 
+if __name__ == '__main__':
+    unittest.main()
 
 class TestBase(unittest.TestCase):
-    """makes some base class ez ref for testing"""
-    b = Base()
-    r = Rectangle()
-    # s = Square()
-
-    """BASE CLASS TESTING"""
+    """TEST: BASE CLASS TESTS"""
     def test_Auto_ID(self):
-        """TEST: IDs are auto-assigned and different"""
-        b1 = Base()
-        b2 = Base()
-        self.assertIsNotNone(b1.id)
-        self.assertIsNotNone(b2.id)
-        self.assertNotEqual(b1.id, b2.id)
+            """TEST: IDs are auto-assigned and different"""
+            b1 = Base()
+            b2 = Base()
+            b3 = Base(11)
+            self.assertIsNotNone(b1.id)
+            self.assertIsNotNone(b2.id)
+            self.assertNotEqual(b1.id, b2.id)
+            self.assertEqual(b1.id, 1)
+            self.assertEqual(b2.id, 2)
+            self.assertEqual(b3.id, 11)
     def Auto_ID_Incrementing(self):
-        """TEST: IDs are incrementing upon new instance creation"""
-        b1 = Base()
-        b2 = Base()
-        b3 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
+            """TEST: IDs are incrementing upon new instance creation"""
+            b1 = Base()
+            b2 = Base()
+            b3 = Base()
+            self.assertEqual(b1.id, 1)
+            self.assertEqual(b2.id, 2)
+            self.assertEqual(b3.id, 3)
     def Base_89(self):
-        """TEST: saving the ID passed exists"""
-        b1 = Base(89)
-        self.assertEqual(b1.id, 89)
+            """TEST: saving the ID passed exists"""
+            b1 = Base(89)
+            self.assertEqual(b1.id, 89)
+    def ID_Type(self):
+        """TEST: IDs asigned are ints"""
+        base = Base(11)
+        self.assertIsInstance(base.id, int)
+    def to_json_empty(self):
+        """TEST: is json empty upon init"""
+        with self.assertRaises(TypeError):
+            b = Base()
+            json_dictionary = Base.to_json_string()
