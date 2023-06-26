45e0ccf4b44ab529c4ede52f32ff2cdd08a4b729
gumquat <gumquat@gmail.com>
2023-06-26 09:47:48 -0500

    select -> show

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- list privileges of two users on localhost
-SELECT GRANTS FOR 'user_0d_1'@'localhost';
-SELECT GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
