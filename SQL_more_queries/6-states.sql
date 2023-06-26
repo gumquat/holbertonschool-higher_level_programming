6a8ca9b2114ebd34154c7de58cd00b88ac4ba278
gumquat <gumquat@gmail.com>
2023-06-26 11:35:07 -0500

    database cahnge

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- creates a database called hbtn_0d_usa and tables called states 
-CREATE database IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states(
