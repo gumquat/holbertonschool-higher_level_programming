260a3a5fb57a9ce142a9ee25e50d735fe651dc01
gumquat <gumquat@gmail.com>
2023-06-26 09:20:50 -0500

    select all change

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Comment
-SELECT GRANTS FOR 'user_0d_1'@'localhost';
-SELECT GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * GRANTS FOR 'user_0d_1'@'localhost';
+SELECT * GRANTS FOR 'user_0d_2'@'localhost';
