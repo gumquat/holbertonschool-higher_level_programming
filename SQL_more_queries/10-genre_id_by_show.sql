a36c3c9b2ac9e10f374dde15f6db700705da7a03
gumquat <gumquat@gmail.com>
2023-06-26 13:11:16 -0500

    join n sort em and uhhh

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in database 
+-- hbtn_0d_tvshows that have >=1 one genre linked
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows 
+    JOIN tv_show_genres 
+      ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres 
+      ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
