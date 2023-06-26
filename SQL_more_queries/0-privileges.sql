1debf6e1bbb57dbc89e3643882049340eca25b9b
gumquat <gumquat@gmail.com>
2023-06-26 08:11:11 -0500

    fixes

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Comment
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * FROM localhost.user WHERE user = 'user_0d_1'
+SELECT * FROM localhost.user WHERE user = 'user_0d_2'
