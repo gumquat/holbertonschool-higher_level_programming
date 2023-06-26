78fb4a47304cd67682d7914cad1f8569c97f8cbf
gumquat <gumquat@gmail.com>
2023-06-26 10:43:56 -0500

    cahnges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- create MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd;
+-- create MySQL server user user_0d_1 and gives them all privileges
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd;
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
