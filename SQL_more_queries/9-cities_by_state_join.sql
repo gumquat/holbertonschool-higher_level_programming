6327e3aac3262b332b91c9d81dd756202f1b618b
gumquat <gumquat@gmail.com>
2023-06-26 12:34:47 -0500

    added a common

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- list all cities in database hbtn_0d_usa
 -- display all cities.id/name/name
 
-SELECT cities.id, cities.name, states.name FROM cities;
+SELECT cities.id, cities.name, states.name, FROM cities;
 JOIN states ON cities.states_id = states.id;
 ORDER BY cities.id ASC;
