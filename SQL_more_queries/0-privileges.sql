0c863b06ec3c6269aa7d18ce6c092262c6a82ca1
gumquat <gumquat@gmail.com>
2023-06-26 09:19:43 -0500

    show -> select

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Comment
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT GRANTS FOR 'user_0d_1'@'localhost';
+SELECT GRANTS FOR 'user_0d_2'@'localhost';
