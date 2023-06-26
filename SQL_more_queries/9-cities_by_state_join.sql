d1dc468be905cf22e01e9aaaa567e06c7512f517
gumquat <gumquat@gmail.com>
2023-06-26 12:55:08 -0500

    right?

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all cities in database hbtn_0d_usa
 -- display all cities.id/name/name
 
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states
+SELECT cities.id, cities.name, states.name FROM cities RIGHT JOIN states
     ON cities.state_id = states.id;
