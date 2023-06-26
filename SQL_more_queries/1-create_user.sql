69464dc61a4f0f690fd97d597950f9019a94f6d9
gumquat <gumquat@gmail.com>
2023-06-26 10:46:00 -0500

    change word

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- create MySQL server user user_0d_1 and gives them all privileges
+-- create MySQL server user user_0d_1 and give them all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd;
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
