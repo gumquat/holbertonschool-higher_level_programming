f93e26f80a30bdde3855540704e59ecf1fb5d307
gumquat <gumquat@gmail.com>
2023-06-26 10:45:28 -0500

    LF

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
 -- create MySQL server user user_0d_1 and gives them all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd;
-GRANT ALL ON *.*
- TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd;
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
