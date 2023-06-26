3a46279601183bedd6266d9ca88fae0779245007
gumquat <gumquat@gmail.com>
2023-06-26 11:16:28 -0500

    changed comment

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
--- creates a table called id not null
+-- creates a table called id not null with id default 1 and a name variable
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT NOT NULL DEFAULT 1,
     name VARCHAR(256)
