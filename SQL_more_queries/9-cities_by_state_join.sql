43901952012faf136414f9b61761dfe302ba6f1b
gumquat <gumquat@gmail.com>
2023-06-26 12:42:31 -0500

    left join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- display all cities.id/name/name
 
 SELECT cities.id, cities.name, states.name; 
-FROM cities JOIN states ON cities.states_id = states.id;
+FROM cities LEFT JOIN states ON cities.states_id = states.id;
 ORDER BY cities.id ASC;
