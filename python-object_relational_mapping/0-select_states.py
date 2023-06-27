5ff3278d4909548e78a0669f33dfe2edfae2bf65
gumquat <gumquat@gmail.com>
2023-06-27 13:13:41 -0500

    pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,10 @@ script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-
-
 def select_states(username, password, database):
     """Connect to MySQL server"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, password=password, db=database)
+    db = MySQLdb.connect(host="localhost", 
+                    port=3306, user=username, password=password, db=database)
 
     """Create a cursor object to execute SQL queries"""
     cursor = db.cursor()
@@ -22,17 +21,19 @@ def select_states(username, password, database):
 
     """Display the results"""
     for row in rows:
-        print(row) # this will not output in the format desired
+        print(row)
 
     """Close the cursor and database connection"""
     cursor.close()
     db.close()
+
 
 """Provide the MySQL username, password, and database name
 username = "your_username"
 password = "your_password"
 database = "hbtn_0e_0_usa"
 """
+
 
 if __name__ == "__main__":
     select_states()
