67618e61800a01abc5bdbe67292a53bd629f94e4
gumquat <gumquat@gmail.com>
2023-06-27 08:29:26 -0500

    13 thru 16 hoooray

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,11 @@
--- list all genres from hbtn_0d_tvshows and display 
--- the number of shows linked to each
-SELECT DISTINCT genre, COUNT(*) AS number_of_shows FROM hbtn_0d_tvshows
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre;
-ORDER BY number_of_shows DESC;
+-- list genres from hbtn_0d_tvshows and the number of shows linked to them
+SELECT DISTINCT
+                tv_genres.name 
+             AS genre,
+                COUNT(*)
+             AS number_of_shows
+           FROM tv_genres
+     INNER JOIN tv_show_genres
+             ON tv_genres.id = tv_show_genres.genre_id
+       GROUP BY genre
+       ORDER BY number_of_shows DESC;
