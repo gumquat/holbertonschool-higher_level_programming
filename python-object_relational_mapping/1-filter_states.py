43d638e763df147d61855b215fc138c7d7852c72
gumquat <gumquat@gmail.com>
2023-06-28 14:56:49 -0500

    name changes

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,16 +11,16 @@ def N_states():
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
 
-    cursor = db.cursor()
+    cur = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE BINARY NAME LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE BINARY NAME LIKE 'N%' ORDER BY id")
 
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
 
-    cursor.close()
+    cur.close()
     db.close()
 
 
