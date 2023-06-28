e0acdcf88c4538f40bdabacfb499a3ac3cf46498
gumquat <gumquat@gmail.com>
2023-06-28 14:50:57 -0500

    broken

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,16 +11,16 @@ def N_states():
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
 
-    cur = db.cursor()
+   # cursor = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE NAME LIKE 'N%' ORDER BY id")
+   # cursor.execute("SELECT * FROM states WHERE NAME LIKE 'N%' ORDER BY id")
 
-    rows = cur.fetchall()
+   # rows = cursor.fetchall()
 
-    for row in rows:
+   # for row in rows:
         print(row)
 
-    cur.close()
+   # cursor.close()
     db.close()
 
 
