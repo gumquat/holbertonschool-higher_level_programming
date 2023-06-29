60ceefd2d14c3b74e7ebe71b866f74da172b94aa
gumquat <gumquat@gmail.com>
2023-06-29 16:46:51 -0500

    comment

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3
+"""
+python file
+contains class def of a state and an instance:
+Base = declarative_base()
+State class:
+inherits from Base Tips
+links to the MySQL table states
+class attribute id that represents a column of an auto-generated, unique integer, cant be null and is a primary key
+class attribute name that represents a column of a string with maximum 128 characters and cant be null
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
