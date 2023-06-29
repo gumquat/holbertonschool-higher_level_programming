f308ab6747d41f7d2998173ca25017718f64f34f
root <gumquat@gmail.com>
2023-06-29 10:10:59 -0700

    :P

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script takes an argument and displays
+all values in the states table from hbtn_0e_0_usa database
+the names matche the argument
+"""
+import MySQLdb
+import sys
+
+
+def list_states_by_name():
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY\
+                 '{}' ORDER BY id".format(sys.argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+    if __name__ == "__main__":
+        list_states_by_name()
