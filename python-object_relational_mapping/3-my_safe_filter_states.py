a12f66d9dc74b9f2b37f02919f89a7236bb6b4ed
root <gumquat@gmail.com>
2023-06-29 13:12:28 -0700

    :P

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ def list_states_by_name_again():
     cursor = db.cursor()
 
     buffer_string = "SELECT * FROM states WHERE name = %s ORDER BY id"
-    cursor.execute(buffer_string, (sys.argv[4]))
+    cursor.execute(buffer_string, (sys.argv[4],))
 
     rows = cursor.fetchall()
     for row in rows:
