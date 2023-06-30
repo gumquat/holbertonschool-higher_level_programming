98233e3d7d3f27f88e086d443421061c728e6d6c
gumquat <gumquat@gmail.com>
2023-06-30 09:38:40 -0500

    thanks chad g petey

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that prints all city objs from database hbtn_0e_14_usa
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), 
+                             pool_pre_ping=True)
+    
+    Base.metadata.create_all(engine)
+
+    """I LOVE LINE LIMITS"""
+    S = sessionmaker(bind=engine)
+    s = S()
+
+    cities = s.query(City).order_by(City.id).all()
+
+    for city in cities:
+        state_name = s.query(State.name).filter_by(id=city.state_id)\
+                     .first()
+
+        print("{}: ({}) {}".format(state_name[0], city.id, city.name))
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python3
+"""
+python file like model_state.py named model_city.py containing the class def of a City.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+"""I dont think these two are needed, as well as the last two lines"""
+from sqlalchemy import create_engine
+import sys
 
 
-class City(Base)
+class City(Base):
+    """city class inherits from base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'),
+                      autoincrement=True, nullable=False)
+
+    engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}\
+                           @localhost:3306/{sys.argv[3]}', pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
