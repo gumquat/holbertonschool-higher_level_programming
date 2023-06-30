6a75aa486fd86fa117bf925210ca80b2fde14406
gumquat <gumquat@gmail.com>
2023-06-30 11:14:00 -0500

    fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,8 +6,10 @@ Base = declarative_base()
 State class:
 inherits from Base Tips
 links to the MySQL table states
-class attribute id that represents a column of an auto-generated, unique integer, cant be null and is a primary key
-class attribute name that represents a column of a string with maximum 128 characters and cant be null
+class attribute id that represents a column of an auto-generated, 
+unique integer, cant be null and is a primary key
+class attribute name that represents a column of a string
+with maximum 128 characters and cant be null
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
