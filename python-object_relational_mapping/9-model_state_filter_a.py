42676d71d3e4258990c7d4d3271b0a699bb92039
gumquat <gumquat@gmail.com>
2023-06-30 11:55:38 -0500

    indention is shitty

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that lists all state objs containing the letter 'a' from database hbtn_0e_6_usa
+script that lists all state objs containing the
+letter 'a' from database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -9,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
+                           .format(sys.argv[1],sys.argv[2],
                                    sys.argv[3]),pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
@@ -19,7 +20,7 @@ if __name__ == "__main__":
     s = S()
 
     states = s.query(State).filter(State.name.like('%a%'))\
-                                 .order_by(State.id).all()
+                           .order_by(State.id).all()
 
     for state in states:
         print(f"{state.id}: {state.name}")
