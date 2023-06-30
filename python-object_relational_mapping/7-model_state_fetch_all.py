62aaa8b9693f462b2c36d370b356b42ef8b4aa4b
gumquat <gumquat@gmail.com>
2023-06-30 07:06:49 -0500

    changes

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""
-script that lists all state objs from database hbtn_0e_6_usa
-"""
+"""script that lists all state objs from database hbtn_0e_6_usa"""
 import sys
 from model_state import Base, State
-"""
-script that lists all state objs from database hbtn_0e_6_usa
-"""
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
