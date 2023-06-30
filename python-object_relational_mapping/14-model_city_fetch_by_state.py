867fb20eecd7ff17ade459e24da7ca070cffe055
gumquat <gumquat@gmail.com>
2023-06-30 14:02:32 -0500

    AHHHHHHHHHH

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-
+    
     Base.metadata.create_all(engine)
 
     """I LOVE LINE LIMITS"""
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     for city in cities:
         state_name = s.query(State.name).filter_by(id=city.state_id)\
                      .first()
-
-        print("{}: ({}) {}".format(state_name[0], city.id, city.name))
+        
+    print("{}:({}) {}".format(state_name[0], city.id, city.name))
