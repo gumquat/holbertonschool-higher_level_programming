f4ac3d973ed546e38e8b8d35a711097d9b27f2ee
gumquat <gumquat@gmail.com>
2023-06-30 11:20:31 -0500

    indentation

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,13 +8,12 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                             pool_pre_ping=True)
-    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                             sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
 
     states = sessionmaker(bind=engine).query(State).order_by(State.id).all()
 
     for state in states:
-        print(f"{state.id}: {state.name}")
+      print(f"{state.id}: {state.name}")
