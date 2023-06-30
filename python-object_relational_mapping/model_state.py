833c6592d2c40ee69566ec29b9638db452b51916
gumquat <gumquat@gmail.com>
2023-06-30 10:51:18 -0500

    AHHHHHHHH

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-=======
 #!/usr/bin/python3
 """
 python file
@@ -22,29 +17,19 @@ import sys
 """
 import all classes who inherit from base before calling Base.metadata.create_all(engine)??
 """
->>>>>>> 98233e3d7d3f27f88e086d443421061c728e6d6c
 
 Base = declarative_base()
 
 class State(Base):
-<<<<<<< HEAD
-=======
     """
     state class
     """
->>>>>>> 98233e3d7d3f27f88e086d443421061c728e6d6c
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-<<<<<<< HEAD
-engine = create_engine('mysql://username:password@localhost:3306/database_name')
-
-Base.metadata.create_all(engine)
-=======
     engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}\
                            @localhost:3306/{sys.argv[3]}', pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
->>>>>>> 98233e3d7d3f27f88e086d443421061c728e6d6c
