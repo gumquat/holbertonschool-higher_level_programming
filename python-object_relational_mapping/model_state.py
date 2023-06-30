f8c9889b6ebd8f588096b52acea51dbfa0e87ea5
gumquat <gumquat@gmail.com>
2023-06-30 11:11:59 -0500

    uhhh

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,11 +14,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 import sys
 
-"""
-import all classes who inherit from base before calling Base.metadata.create_all(engine)??
-"""
 
 Base = declarative_base()
+
 
 class State(Base):
     """
@@ -29,7 +27,3 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-    engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}\
-                           @localhost:3306/{sys.argv[3]}', pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
