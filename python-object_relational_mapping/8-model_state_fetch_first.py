9fec623a489a77ecfb2a0c7547f2959cf979b036
gumquat <gumquat@gmail.com>
2023-06-30 11:43:35 -0500

    AHHHHHHHHHH

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,14 +9,17 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                             pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     
     Base.metadata.create_all(engine)
 
-    state = sessionmaker(bind=engine).query(State).order_by(State.id).first()
+    S = sessionmaker(bind=engine)
+    s = S()
+
+    state = s.query(State).order_by(State.id).first()
 
     if state is None:
         print("Nothing")
     else:
-        print(f"{state.id}: {state.name}")
+                print(f"{state.id}: {state.name}")
