0680337934524f647b48a96912f65abd1f643d40
gumquat <gumquat@gmail.com>
2023-06-30 07:11:29 -0500

    AHHHHHHB ETTY

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-script that prints the first state 
-    obj from database hbtn_0e_6_usa
+script that prints the first state obj from database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
