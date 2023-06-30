86fa0e2356c69254158f1bda89f5eb753f296f70
gumquat <gumquat@gmail.com>
2023-06-30 11:15:10 -0500

    white space

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ Base = declarative_base()
 State class:
 inherits from Base Tips
 links to the MySQL table states
-class attribute id that represents a column of an auto-generated, 
+class attribute id that represents a column of an auto-generated,
 unique integer, cant be null and is a primary key
 class attribute name that represents a column of a string
 with maximum 128 characters and cant be null
