36250d64da41152f6c7733c0c88cd2f8146d198b
gumquat <gumquat@gmail.com>
2023-07-31 11:36:49 -0500

    new

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-//Creates a constant variable called myVar with the value "JavaScript is amazing"
-//Uses the console.log() function to print the value of myVar to the console
 const myVar = "JavaScript is amazing";
 console.log(myVar);
