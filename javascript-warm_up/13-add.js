64eefcba4c660720453e8879c7237fe4a17a65ef
gumquat <gumquat@gmail.com>
2023-07-31 13:14:13 -0700

    :P

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-const add = (x, y) => {
-  return x + y;
-};
-const sum = add(10, 20);
-console.log(sum);
+add = require('./13-add').add
+console.log(add(4, 85);
 
