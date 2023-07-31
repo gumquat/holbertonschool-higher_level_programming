cccb808f0f12d1b104281627b558a97de27f87e8
gumquat <gumquat@gmail.com>
2023-07-31 12:03:57 -0700

    :P

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const argv = require('process').argv;
+
 if (argv.length < 3) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argv.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
 
