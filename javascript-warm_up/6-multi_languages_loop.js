69cd70dbec51ab8d73b837e437da74624e55e9f0
gumquat <gumquat@gmail.com>
2023-07-31 12:29:06 -0700

    :P

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let counter;
+const STRING_ARRAY = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (counter = 0; counter < STRING_ARRAY.length; counter++) {
+  console.log(STRING_ARRAY[counter]);
+}
