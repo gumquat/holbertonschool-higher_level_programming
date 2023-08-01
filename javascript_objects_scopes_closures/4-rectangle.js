456cc08d5ad4fa0711b0e4c082cf1648db4cc158
gumquat <gumquat@gmail.com>
2023-08-01 10:34:04 -0700

    :P

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let yCounter;
+    for (yCounter = 0; yCounter < this.height; yCounter++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
