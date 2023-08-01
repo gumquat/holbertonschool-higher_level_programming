7259032c1bf1a21eb3853398e314ec7eb27a678b
gumquat <gumquat@gmail.com>
2023-08-01 10:42:08 -0700

    :P

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,23 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
-const r1 = new Rectangle(2, 3);
-r1.print();
-const r2 = new Rectangle(10, 5);
-r2.print();
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let xCounter;
+    let yCounter;
+    let output = '';
+    for (xCounter = 0; xCounter < this.width; xCounter++) {
+      output += 'X';
+    }
+    for (yCounter = 0; yCounter < this.height; yCounter++) {
+      console.log(output);
+    }
+  }
+}
+
+module.exports = Rectangle;
--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -7,3 +7,4 @@ class Square extends Rectangle {
   }
 }
 module.exports = Square;
+
