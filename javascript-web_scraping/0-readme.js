b8f6f8a53a2f9364584a09a709e227c3c1c17b1a
gumquat <gumquat@gmail.com>
2023-08-04 09:29:12 -0700

    :P

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,19 +3,12 @@ const fs = require('fs');
 
 function readFile (filePath) {
   fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err){
-	    console.log(err);
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
     }
-    console.log(data);
   });
 }
 
-// if no filepath is provided this looks for one
-// and then reads the file as normal using the readFile func
-const filePath = process.argv[2];
-
-if (!filePath) {
-  console.error(err);
-} else {
-  readFile(filePath);
-}
+readFile();
