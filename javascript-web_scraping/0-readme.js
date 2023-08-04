4ffb39de91c1c162b30882a00734fa70ecd70545
gumquat <gumquat@gmail.com>
2023-08-04 09:04:53 -0700

    :P

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,17 +3,16 @@ const fs = require('fs');
 
 function readFile (filePath) {
   fs.readFile(filePath, 'utf-8', (err, data) => {
-
-if (err) {
-	console.error(err);
-} else {
-	console.log(data);
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
     }
   });
 }
 
-//if no filepath is provided this looks for one
-//and then reads the file as normal using the readFile func
+// if no filepath is provided this looks for one
+// and then reads the file as normal using the readFile func
 const filePath = process.argv[2];
 
 if (!filePath) {
@@ -21,4 +20,3 @@ if (!filePath) {
 } else {
   readFile(filePath);
 }
-
