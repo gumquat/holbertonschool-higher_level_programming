b43343afc98f73f95f581663d2c251414341d0c0
gumquat <gumquat@gmail.com>
2023-08-04 11:06:05 -0700

    :P

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,13 +2,13 @@
 const fs = require('fs');
 
 function writeFile (filePath, content) {
-  fs.writeFile(filePath, content, 'utf-8', (err) => {
-	if (err) {
-		console.log(err);
-	}
-	}
-	);
-	}
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+if (err) {
+console.log(err);
+}
+}
+);
+}
 
 const filePath = process.argv[2];
 const content = process.argv[3];
