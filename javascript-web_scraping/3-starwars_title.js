e2e4edde5951938e0a3c145aff18a0a05850c381
gumquat <gumquat@gmail.com>
2023-08-04 12:54:13 -0700

    :P

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+function getMovieTitle (movieId) {
+  const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  request.get(url, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const movie = JSON.parse(body);
+      console.log(`Title: ${movie.title}`);
+    }
+  });
+}
+
+const movieId = process.argv[2];
+
+getMovieTitle(movieId);
