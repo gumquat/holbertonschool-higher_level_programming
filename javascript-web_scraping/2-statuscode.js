7ea9bd2868bb566ea87ef90b9cada5953c27aa0c
gumquat <gumquat@gmail.com>
2023-08-04 11:55:31 -0700

    :P

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 
+function getRequestStatusCode (url) {
+  request.get(url, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('code: ' + response.statusCode);
+  });
+}
+
 const url = process.argv[2];
 
-request.get(url, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('code: ' + response.statusCode);
-});
+getResquestStatusCode(url);
