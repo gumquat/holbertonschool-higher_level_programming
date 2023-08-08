8fd701e95726096c189b6c4c33f3797646e7e8fd
gumquat <gumquat@gmail.com>
2023-08-08 16:46:44 +0000

    :P

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,39 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
+let counter = 0;
 
-request.get(url, (err, response, body) => {
-  if (err) {
-    console.log(err);
+function get(url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else if (res.statusCode !== 200) {
+        reject(new Error(`Request failed with status: ${res.statusCode}`));
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+async function countCharacters() {
+  try {
+    const body = await get(url); // async operation
+    const data = JSON.parse(body).results;
+    
+    for (const film of data) {
+      for (const character of film.characters) {
+        if (character.includes('/18/')) {
+          counter++;
+        }
+      }
+    }
+    
+    console.log(counter);
+  } catch (error) {
+    console.error(error.message);
   }
-  if (response.statusCode !== 200) {
-    console.log(err);
-    const counter = JSON.parse(body).results.reduce((count, film) =>
-      count + film.characters.filter(character => character.includes('/18/')).length, 0);
-  }
-  console.log(counter);
-});
+}
+
+countCharacters();
