a3cf8308953c8032abb45d70ba9bb18551161fe6
gumquat <gumquat@gmail.com>
2023-08-08 16:56:22 +0000

    :P

--- javascript-web_scraping/5-request_Store.js
+++ javascript-web_scraping/5-request_Store.js
@@ -1,16 +1,43 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
-const fs = require('fs');
 const url = process.argv[2];
-const fileName = process.argv[3];
+let counter = 0;
 
-request.get(url, (err, response, body) => {
-  if (err) return console.log(err);
-  if (response.statusCode !== 200) {
-    return console.log(err);
+// Create promisify function
+function promisify (fn) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, res) => {
+        if (err) return reject(err);
+        resolve(res);
+      });
+    });
+  };
 }
-  fs.writeFile(fileName, body, 'utf-8', err => {
-    if (err) console.log(err);
-  });
-});
+
+async function processRequest () {
+  try {
+    // Promisified version of request.get
+    const get = promisify(request.get);
+    const { body, statusCode } = await get(url);
+
+    if (statusCode !== 200) {
+      console.error('Request failed with status:', statusCode);
+      return;
+    }
+
+    const data = JSON.parse(body).results;
+    for (const film of data) {
+      for (const character of film.characters) {
+        if (character.includes('/18/')) {
+          counter += 1;
+        }
+      }
+    }
+    console.log(counter);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+processRequest();
